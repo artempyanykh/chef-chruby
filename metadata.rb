68a4a560a51152df665d9af07c143830e19ac765
Artem Pyanykh <artem.pyanykh@gmail.com>
2014-07-04 15:49:50 +0400

    Ignore chruby-version, use patched branch instead of replaced chruby-exec

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'artem.pyanykh@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures chruby'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 supports         'ubuntu'
 
 depends           'ark'
--- providers/default.rb
+++ providers/default.rb
@@ -5,15 +5,8 @@ action :install do
     Chef::Log.info("chruby already installed. Skipping installation...")
   else
     ark "chruby" do
-      url "https://github.com/postmodern/chruby/archive/v#{version}.tar.gz"
+      url "https://github.com/ArtemPyanykh/chruby/archive/chruby-exec-sourcing.zip"
       action :install_with_make
-    end
-
-    # Replace chruby-exec with the one with fixed sourcing
-    cookbook_file "chruby-exec" do
-      path '/usr/local/bin/chruby-exec'
-      mode 0755
-      cookbook "chruby"
     end
   end
 end
